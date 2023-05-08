a4c3200dc4509b156ac7c14f78ae250c19812a77
Mike <nhj25252@gmail.com>
2023-05-08 23:44:03 +0900

    build Tree factory

--- binarySearch.js
+++ binarySearch.js
@@ -1,6 +1,24 @@
-function Node(data = null, leftChild = null, rightChild = null) {
-  return { data, leftChild, rightChild };
+function Node(data = null, left = null, right = null) {
+  return { data, left, right };
 }
 
 const node = Node();
 console.log(node);
+
+const root = null;
+
+function Tree() {
+  return {
+    root,
+  };
+}
+
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+function buildTree(array) {
+  const tree = Tree();
+
+  return { tree }; // same as returning level 0 root
+}
+
+console.log(buildTree(array));

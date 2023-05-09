77a3868eefae282f1fa6c632c268ffe5e39b5bda
Mike <nhj25252@gmail.com>
2023-05-09 11:57:25 +0900

    build BST

--- binarySearch.js
+++ binarySearch.js
@@ -2,42 +2,58 @@ function Node(data = null, left = null, right = null) {
   return { data, left, right };
 }
 
-const node = Node();
-console.log(node);
+const root = Node();
+console.log(root);
 
-const root = null;
+// const root = null;
 
 // a tree returns the root
-function Tree() {
+function Tree(root = null) {
   return {
     root,
   };
 }
 
-let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
 
 array.sort((a, b) => a - b);
 
 const sortedArray = [...new Set(array)];
 console.log(sortedArray);
+
 // end = end index of array
-const tree = Tree();
+// if not specified when called..
+function buildBST(arr, start = 0, end = arr.length - 1) {
+  /* Base Case */
 
-function buildTree(array) {
-  let start = 0;
-  let end = array.length - 1; // end index
-
-  if (start > end) return null; // base case
-
-  let mid = Math.ceil((start + end) / 2); // rounds up
-  const node = Node(array[mid]); // set the middle element as root node of left subarray
-  // data is set to the index's element
-
-  node.left = buildTree(array, start, mid - 1);
-  node.right = buildTree(array, mid + 1, end);
-  console.log(mid);
-
+  if (start > end) {
+    return null;
+  }
+  /* Get the middle element and make it root */
+  let mid = Math.floor((start + end) / 2);
+  let node = Node(arr[mid]);
+  console.log("node", node);
+  node.left = buildBST(arr, start, mid - 1);
+  console.log("node.left", node.left);
+  node.right = buildBST(arr, mid + 1, end);
+  console.log("node.right", node.right);
   return node;
 }
 
-console.log(buildTree(sortedArray));
+console.log(buildBST(sortedArray));
+// const tree = Tree();
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+console.log(prettyPrint(buildBST(sortedArray)));

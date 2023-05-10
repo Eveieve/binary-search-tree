c7ddee6b38be4fe91eed810fa31d221fbd54f93e
Mike <nhj25252@gmail.com>
2023-05-10 15:16:18 +0900

    clean up

--- binarySearch.js
+++ binarySearch.js
@@ -2,27 +2,15 @@ function Node(data = null, left = null, right = null) {
   return { data, left, right };
 }
 
-// const root = Node();
-// console.log(root);
-
-// const root = null;
-
 let array = [1, 2, 3, 4, 5, 6];
-
 array.sort((a, b) => a - b);
-
 const sortedArray = [...new Set(array)];
-console.log(sortedArray);
 
-// end = end index of array
-// if not specified when called..
 function buildBST(arr, start = 0, end = arr.length - 1) {
-  /* Base Case */
-
   if (start > end) {
     return null;
   }
-  /* Get the middle element and make it root */
+
   let mid = Math.floor((start + end) / 2);
   let node = Node(arr[mid]);
   console.log("node", node);
@@ -40,8 +28,7 @@ class Tree {
 
 let BST = buildBST(sortedArray);
 
-// let root = null;
-
+// take in the tree/root
 function insert(root, data) {
   if (root == null) {
     root = Node(data);
@@ -52,13 +39,16 @@ function insert(root, data) {
   return root;
 }
 
-console.log(insert(BST, 10));
-console.log(insert(BST, 9));
+// console.log(insert(BST, 10));
+// console.log(insert(BST, 9));
+
+console.log(BST);
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
   }
+  // console.log(node);
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
